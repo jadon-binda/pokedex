937195d0659675db21ddd87dfd7f2dd0f2d1e5a5
JB <jadon.binda@gmail.com>
2023-11-03 00:06:19 -0300

    implement function to search for pokemons in input search

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+
+let searchPokemon = 1;
 
 const responseAPI = async (pokemon) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
   const data = await response.json();
   return data;
-}
+};
 
 const renderPokemon = async (pokemon) => {
   const data = await responseAPI(pokemon);
@@ -14,4 +18,12 @@ const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = name;
   pokemonNumber.innerHTML = id;
   pokemonImage.src = sprites.versions['generation-v']['black-white'].animated.front_default;
-}
+  input.value = '';
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value);
+});
+
+renderPokemon(searchPokemon);
