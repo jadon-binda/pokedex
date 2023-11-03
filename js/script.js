8c9baf4c4af99d78c438dcd215b9b825430a5fac
JB <jadon.binda@gmail.com>
2023-11-02 23:39:25 -0300

    change function to get pokeAPI response to an async function and create function to render Pokemon data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-const responseAPI = (pokemon) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      return data;
-    })
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+
+const responseAPI = async (pokemon) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await response.json();
+  return data;
 }
 
+const renderPokemon = async (pokemon) => {
+  const data = await responseAPI(pokemon);
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+}
+
+renderPokemon('6');
