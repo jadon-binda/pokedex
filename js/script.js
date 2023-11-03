b6a7ba60cd9a4ada5de878f03ed70399a11e9198
JB <jadon.binda@gmail.com>
2023-11-02 23:52:16 -0300

    implement function to render pokemon data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
 
 const responseAPI = async (pokemon) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -9,8 +10,8 @@ const responseAPI = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
   const data = await responseAPI(pokemon);
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
+  const { name, id, sprites } = data;
+  pokemonName.innerHTML = name;
+  pokemonNumber.innerHTML = id;
+  pokemonImage.src = sprites.versions['generation-v']['black-white'].animated.front_default;
 }
-
-renderPokemon('6');
